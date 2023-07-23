dfc87ab23406db8f5528884f2a040c97c6e49994
anaconda77 <62774721+anaconda77@users.noreply.github.com>
2023-07-24 00:03:02 +0900

    Feat.main.dart 수정
    DB 관련 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:GroupSettlement/firebase_options.dart';
+import 'package:get/get.dart';
+import 'dart:async';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
